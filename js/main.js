1f3c5bbf99152e6e0b1174d37a81c07cc43f5b20
gygykan <nedeleagnicolae81@gmail.com>
2022-11-05 14:46:52 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//alert("Hello, world");
+
+var nume = "Ion";
+console.log(nume);
+
+let prenume = "Marcel";
+console.log(prenume);
+
+var nume = "Ionel";
+console.log(nume);
+
+
+prenume = "George";
+console.log(prenume);
+
+let varsta = 18;
+
+console.log(typeof nume);
+console.log(typeof prenume);
+console.log(typeof varsta);
